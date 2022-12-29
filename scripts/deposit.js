7b4458f9b8277df8a1c705a4d0f264476f42fcf2
tolak <kalot.wang@gmail.com>
2022-12-29 23:25:23 +0800

    adjust request data structure

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -8,39 +8,52 @@ const USDC_ON_MOONBEAM = '0x8f552a71EFE5eeFc207Bf75485b356A0b3f01eC9'
 const HANDLER_ON_GOERLI = '0xbEA1C40ecf9c4603ec25264860B9b6623Ff733F5'
 const TEST_WORKER = '0xf60dB2d02af3f650798b59CB6D453b78f2C1BC90'
 
+
+/******** OperationJson definition in Rust **************
+struct OperationJson {
+    op_type: String,
+    source_chain: String,
+    dest_chain: String,
+    spend_asset: Address,
+    receive_asset: Address,
+    dex: String,
+    fee: String,
+    cap: String,
+    flow: String,
+    impact: String,
+    spend: String,
+}
+******************************************************/
+
 function createRequest() {
-    let request = [{
-        'type': 'swap',
-        'data': {
-            'spend_asset': PHA_ON_GOERLI,
-            'receive_asset': USDC_ON_GOERLI,
-            'chain': 'Ethereum',
-            'dex': 'UniswapV2',
-            'cap': 0,
-            'flow': 0,
-            'impact': 0,
-            'b0': null,
-            'b1': null,
-            // Spend 100 PHA to swap USDC
-            'spend': '100000000000000000000'
-        }
+    let operations = [{
+        'op_type': 'swap',
+        'source_chain': 'Ethereum',
+        'dest_chain': 'Ethereum',
+        'spend_asset': PHA_ON_GOERLI,
+        'receive_asset': USDC_ON_GOERLI,
+        'dex': 'UniswapV2',
+        'fee': "0",
+        'cap': "0",
+        'flow': "0",
+        'impact': "0",
+        // Spend 100 PHA to swap USDC
+        'spend': '100000000000000000000'
     }, {
-        'type': 'bridge',
-        'data': {
-            'from': USDC_ON_GOERLI,
-            'source_chain': 'Ethereum',
-            'to': USDC_ON_MOONBEAM,
-            'dest_chain': 'Moonbeam',
-            'fee': 0,
-            'cap': 0,
-            'flow': 0,
-            'b0': null,
-            'b1': null,
-            // USDC has decimas 6, and assume PHA price is 0.12 USDC
-            'amount': '12000000'
-        }
+        'op_type': 'bridge',
+        'source_chain': 'Ethereum',
+        'dest_chain': 'Moonbeam',
+        'spend_asset': USDC_ON_GOERLI,
+        'receive_asset': USDC_ON_MOONBEAM,
+        'dex': '',
+        'fee': "0",
+        'cap': "0",
+        'flow': "0",
+        'impact': "0",
+        // USDC has decimas 6, and assume PHA price is 0.12 USDC
+        'spend': '12000000'
     }]
-    return JSON.stringify(request)
+    return JSON.stringify(operations)
 }
 
 async function main() {
@@ -55,13 +68,13 @@ async function main() {
     wallet
   )
 
-  // tx: 0x075d27a64fbb000ed76ae9515e0feef842043ab20ee14e64ba06457540f31335
+  // tx: 0xb27219debadfaca3ad50c4dcfa8e913921d21795f5f5e53c178fa941bf3b0353
   let tx = await handler.deposit(
     PHA_ON_GOERLI,
     '100000000000000000000',
     '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d',
     TEST_WORKER,
-    '0x0000000000000000000000000000000000000000000000000000000000000001',
+    '0x0000000000000000000000000000000000000000000000000000000000000007',
     createRequest()
   )
   console.log(`Deposit tx: ${tx.hash}`)
